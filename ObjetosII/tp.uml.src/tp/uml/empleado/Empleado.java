7cf2d6602851311cd96afdb3738bbc9caeedfde0
Evantex <evantedev@gmail.com>
2024-03-28 21:08:32 -0300

    Falta implementar hashMap y métodos de hash

---
+++ ObjetosII/tp.uml.src/tp/uml/empleado/Empleado.java
@@ -0,0 +1,90 @@
+package tp.uml.empleado;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import tp.uml.empresa.Recibo;
+import tp.uml.empresa.Concepto;
+import java.util.Collection;
+import java.time.Period;
+
+public abstract class Empleado
+{
+	protected String nombre;
+	protected String direccion;
+	protected String estadoCivil;
+	protected LocalDate fechaNacimiento;
+	protected int sueldoBasico;
+	// protected List<Recibo> recibosDeHaberes = new ArrayList<>();
+	
+
+	/*
+	public void agregarRecibo(Recibo recibo)
+	{
+		this.recibosDeHaberes.add(recibo);
+	}
+	*/
+	
+	/*
+	public Recibo verRecibo(int nroRecibo)
+	{
+		if( this.recibosDeHaberes.size() < nroRecibo )
+		{
+			throw new IllegalArgumentException("Indice invalido");
+		}
+		return this.recibosDeHaberes.get(nroRecibo);
+	}
+	*/
+
+	
+	public String nombre()
+	{
+		return nombre;
+	}
+	
+	
+	public String getDireccion()
+	{
+		return this.direccion;
+	}
+	
+	
+	public LocalDate fechaNacimiento()
+	{
+		return fechaNacimiento;
+	}
+	
+	
+	public int edad()
+	{
+		LocalDate fechaActual = LocalDate.now();
+		Period diferencia = Period.between(fechaNacimiento, fechaActual);
+		return diferencia.getYears();
+	}
+	
+	
+	public int retenciones()
+	{
+		return this.obraSocial() + this.aportesJubilatorios();
+	}
+	
+	
+	public abstract int sueldoBruto();
+	protected abstract int obraSocial();
+	protected abstract int aportesJubilatorios();
+	protected abstract List<Concepto> getConceptos();
+	
+	
+	public int sueldoNeto()
+	{
+		return this.sueldoBruto() - this.retenciones();
+	}
+	
+	
+	public Recibo  generarRecibo()
+	{
+		return new Recibo( this.nombre, this.direccion, LocalDate.now(), this.sueldoBruto(), this.sueldoNeto(), this.getConceptos() );
+	}
+	
+	
+	
+}
--- ObjetosII/tp.uml.src/tp/uml/empresa/Empresa.java
+++ ObjetosII/tp.uml.src/tp/uml/empresa/Empresa.java
@@ -1,6 +1,81 @@
 package tp.uml.empresa;
+import tp.uml.empleado.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class Empresa 
 {
+	private String nombre;
+	private int cuit;
+	private Collection<Empleado> listaDeEmpleados = new ArrayList<Empleado>();
+	private Map<Empleado, List<Recibo>> recibos = new HashMap<>();
+	
+	public Collection<Empleado> getListaDeEmpleados()
+	{
+		return this.listaDeEmpleados;
+	}
+	
+	
+	public void agregarEmpleado(Empleado empleado)
+	{
+		this.listaDeEmpleados.add(empleado);
+	}
 
+	
+	
+	public void generarRecibos()
+	// Hacerlo todo en el mismo hash, utilizando containkey.
+	{
+		for( Empleado empleado: listaDeEmpleados )
+		{
+			List<Recibo> listaRecibos = recibos.get(empleado);
+			
+			recibos.get(empleado).add(empleado.generarRecibo());
+			// recibos.put( empleado, empleado.generarRecibo() );
+			recibos.
+		}
+	}
+	
+	public int getTotalSueldosNeto()
+	{
+		int monto = 0;
+		for( Empleado empleado: listaDeEmpleados )
+		{
+			monto += empleado.sueldoNeto();
+		}
+		return monto;
+	}
+	
+	public int getTotalSueldosBruto()
+	{
+		int monto = 0;
+		for( Empleado empleado: listaDeEmpleados )
+		{
+			monto += empleado.sueldoBruto();
+		}
+		return monto;
+	}
+	
+	public int getTotalRetenciones()
+	{
+		int monto = 0;
+		for( Empleado empleado: listaDeEmpleados )
+		{
+			monto += empleado.retenciones();
+		}
+		return monto;
+	}
+	
+	public Empresa(String nombre, int cuit)
+	{
+		this.nombre = nombre;
+		this.cuit = cuit;
+	}
+	
+	
 }
